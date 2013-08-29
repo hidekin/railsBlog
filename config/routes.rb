7d0ee22dfa807e2f94eb284cd6b274f84499c969
Matthias Brunisholz <hidekin1982@gmail.com>
2013-08-29 20:53:03 +0200

    first uplodad

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+QuickBlog::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
+  root :to => 'posts#index'
+
+  resources :posts do
+    resources :comments, :only => [:create]
+  end
+
+  get '/about', :to => 'pages#about'
+
+  match '/404', :to => 'errors#not_found'
+  match '/500', :to => 'errors#internal_error'
+  match '/422', :to => 'errors#unprocessable_entity'
+
+end
